3c5b80422aeab0a1fa87ce49a27df4cacfafc29d
cabrerajaime <cabrerajaime877@gmail.com>
2023-03-06 10:45:13 -0600

    Funcionalidad para insertar nuevos datos sobre la tabla

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import sys
 
 from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, \
-    QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem
+    QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, \
+    QDialog, QVBoxLayout, QComboBox
 
 import sqlite3
 
@@ -18,6 +19,8 @@ class MainWindow(QMainWindow):
 
         # Añadir acciones a los ítems que conforman el menu
         add_student_action = QAction("Add Student", self)
+        # Se llama al método insert
+        add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
         about_action = QAction("About", self)
@@ -25,19 +28,78 @@ class MainWindow(QMainWindow):
 
         self.table = QTableWidget()
         self.table.setColumnCount(4)
-        self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.table.setHorizontalHeaderLabels(
+            ("Id", "Name", "Course", "Mobile"))
+        # Quitar visibilidad a los indices verticales de la tabla
         self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
     def load_data(self):
         connection = sqlite3.connect("database.db")
         result = connection.execute("SELECT * FROM students")
+        #  Ese método reinicia la tabla y toma los datos como nuevos para evitar que se repitan.
         self.table.setRowCount(0)
+        # Reservar una fila para comenzar a insertar columnas sobre ella
         for row_number, row_data in enumerate(result):
             self.table.insertRow(row_number)
+            # Extraer datos columna por columna sobre fila
             for column_number, data in enumerate(row_data):
-                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+                self.table.setItem(row_number, column_number,
+                                   QTableWidgetItem(str(data)))
+            # Termina ciclo (las columnas se han llenado), sale y se inserta otra fila
         connection.close()
+
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
+
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Insert Student Data")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        # Widget para insertar estudiante
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+
+        # Widget para seleccionar el curso
+        self.course_name = QComboBox()
+        courses = ["Biology", "Math", "Astronomy", "Physics"]
+        self.course_name.addItems(courses)
+        layout.addWidget(self.course_name)
+
+        # Widget para agregar número de telefono
+        self.mobile_number = QLineEdit()
+        self.mobile_number.setPlaceholderText("Mobile")
+        layout.addWidget(self.mobile_number)
+
+        # Agregar botón para enviar
+        button = QPushButton("Register")
+        button.clicked.connect(self.add_student)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+    def add_student(self):
+        name = self.student_name.text()
+        course = self.course_name.itemText(self.course_name.currentText())
+        mobile = self.mobile_number.text()
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)",
+                       (name, course, mobile))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        # Refrescar la tabla de estudiantes en el menu principal
+        menu_window.load_data()
 
 
 app = QApplication(sys.argv)
